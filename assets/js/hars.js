cfa470d337494a0d60f328a316434aef89316de1
Nicolás Robles <nicopj20@gmail.com>
2020-11-11 17:27:03 -0300

    Carga de planillas en el LocalStorage

--- assets/js/hars.js
+++ assets/js/hars.js
@@ -31,6 +31,7 @@ var ansiedadSomatica=0;
 var puntajeTotalHars=0;
 
 
+
 //Variables DOM
 const descripcion = document.getElementById("descripcionHars");
 const formPaciente = document.getElementById("formPaciente")
@@ -41,7 +42,7 @@ const nivelesDeAnsiedadHtml=document.getElementById("nivelesAnsiedad");
 const resultadoPrincipalHtml= document.getElementById("resultadoPrincipal");
 const resultadoAPsiquicaHtml=document.getElementById("resultadoPsi");
 const resultadoASomaticaHtml=document.getElementById("resultadoSom");
-    //La Sig Variable, es un array con el nombre de las clases de los imputs radio
+//La Sig Variable, es un array con el nombre de las clases de los imputs radio
 const clasesRadiosRespuesta = ["gradoAnsiedadI1","gradoAnsiedadI2","gradoAnsiedadI3","gradoAnsiedadI4","gradoAnsiedadI5","gradoAnsiedadI6","gradoAnsiedadI7","gradoAnsiedadI8","gradoAnsiedadI9","gradoAnsiedadI10","gradoAnsiedadI11","gradoAnsiedadI12","gradoAnsiedadI13","gradoAnsiedadI14"]
 
 //Creo los objetos de Scope Global
@@ -50,149 +51,164 @@ var resultadosEscalaHars= new EscalaHars();
 
 
 
+/*~~~EVENTOS~~~*/
+    //Asignada a un boton para cargar los datos del paciente
+    function cargarPaciente(){
+
+        descripcion.classList.replace('d-block','d-none');
+        formPaciente.classList.replace('d-none','d-block');
+    }
+
+    //Asignada a un boton para comenzar el test
+    function comenzarHars(){
+        logPaciente();
+        formPaciente.classList.replace('d-block','d-none');
+        preguntasHars.classList.replace('d-none','d-block');
+    }
+
+    //Asignada a un boton para obtener resultados, mostrarlos
+    //y subir todos los datos al LocalStorage (mi DB provisoria)
+    function mostrarResultados(){
+        respuestasHars();
+        harsResultados(resultadosEscalaHars);
+        guardarPlanilla();
+        preguntasHars.classList.replace('d-block','d-none')
+        resultadosHarsHtml.classList.replace('d-none','d-block')
+    }
+/*~~~~~~~~~~~~*/
+
+
 /*==================FUNCIONES DEL TEST HARS=================*/
 
-function cargarPaciente(){
+    /*Funcion para que el usuario cargue los datos del paciente*/
+    function logPaciente(){
+        paciente.nombre = document.getElementById("inputNombre").value;
+        paciente.edad = document.getElementById("inputEdad").value;
+        paciente.sexo = document.getElementById("inputSexo").value;
+        paciente.eMail = document.getElementById("inputEmail").value;
+        paciente.hars = null;
 
-    descripcion.classList.replace('d-block','d-none');
-    formPaciente.classList.replace('d-none','d-block');
-}
-//Funcion asignada a un botton para comenzar el test
-//Por ahora solo esconde la descripcion y muestra las preguntas
-function comenzarHars(){
-    logPaciente();
-    formPaciente.classList.replace('d-block','d-none')
-    preguntasHars.classList.replace('d-none','d-block')
-}
+        sessionStorage.setItem('nombrePaciente',paciente.nombre);
+    }
 
 
-function mostrarResultados(){
-    respuestasHars();
-    harsResultados(resultadosEscalaHars);
-    preguntasHars.classList.replace('d-block','d-none')
-    resultadosHarsHtml.classList.replace('d-none','d-block')
-}
+    //Esta función simula una base de datos en la nube, hasta que aprenda algún metodo mejor de almacenamiento
+    function guardarPlanilla(){
 
+        let numeroDePlanillas = localStorage.getItem('Cantidad de Planillas');
+        let jsonPaciente = JSON.stringify(paciente);
 
-/*Funcion para que el usuario cargue los datos del paciente*/
+        numeroDePlanillas = numeroDePlanillas + 1;
 
-function logPaciente(){
-    paciente.nombre = document.getElementById("inputNombre").value;
-    paciente.edad = document.getElementById("inputEdad").value;
-    paciente.sexo = document.getElementById("inputSexo").value;
-    paciente.eMail = document.getElementById("inputEmail").value;
-    paciente.hars = null;
+        localStorage.setItem('Paciente Nro'+numeroDePlanillas , jsonPaciente)
+        localStorage.setItem('Cantidad de Planillas',numeroDePlanillas)
+    }
 
-    sessionStorage.setItem('nombrePaciente',paciente.nombre);
-    console.log(paciente.nombre+"\n"+paciente.edad+"\n"+paciente.sexo+"\n"+paciente.eMail);
-}
+    //~~ESTA FUNCION RECORRE TODOS LOS IMPUTS Y ASIGNA LOS VALORES A UN ARRAY EN LA POSICION CORRESPONDIENTE~~
+    function respuestasHars(){
+        //Este for recorre un array con los nombre de las 14 clases que van a tener los 5 radios dentro de cada uno de los 14 item
+        //y asigna el nombre de cada clase a la variable RadiosRespuesta
+        for (i=0;i<clasesRadiosRespuesta.length;i++){
+            let radiosRespuesta = document.getElementsByClassName(clasesRadiosRespuesta[i])
+            let numerodeItem = i+1 ;//Esta var es solamente para ayudarme en la consola
+        
+            // Como cada clase posee 5 radios con su nombre, con este for, recorro los 5 imputs
+            //y con un if me encargo de capturar el valor del radio que este tildado y agregarlo al array items
+            for (j=0; j<5; j++){
+                if (radiosRespuesta[j].checked){
+                    console.log("El item numero "+numerodeItem+" tuvo la respuesta "+radiosRespuesta[j].value);
+                    items[i] = radiosRespuesta[j].value;
+                }
+            }
 
-
-//~~ESTA FUNCION RECORRE TODOS LOS IMPUTS Y ASIGNA LOS VALORES A UN ARRAY EN LA POSICION CORRESPONDIENTE~~
-function respuestasHars(){
-    //Este for recorre un array con los nombre de las 14 clases que van a tener los 5 radios dentro de cada uno de los 14 item
-    //y asigna el nombre de cada clase a la variable RadiosRespuesta
-    for (i=0;i<clasesRadiosRespuesta.length;i++){
-        let radiosRespuesta = document.getElementsByClassName(clasesRadiosRespuesta[i])
-        let numerodeItem = i+1 ;//Esta var es solamente para ayudarme en la consola
-    
-        // Como cada clase posee 5 radios con su nombre, con este for, recorro los 5 imputs
-        //y con un if me encargo de capturar el valor del radio que este tildado y agregarlo al array items
-        for (j=0; j<5; j++){
-            if (radiosRespuesta[j].checked){
-                console.log("El item numero "+numerodeItem+" tuvo la respuesta "+radiosRespuesta[j].value);
-                items[i] = radiosRespuesta[j].value;
+            //Este if sirve para validar que todos los items tengan un radio checkeado
+            //En caso de no no ser así llamo a la funcion alerta, que abre una ventana de alerta
+            if(items[i] == null){
+                console.log("El item numero "+numerodeItem+" no tiene respuesta")
+                alertaValidacionRadios();
+                break
             }
         }
+        console.log(items)
 
-        //Este if sirve para validar que todos los items tengan un radio checkeado
-        //En caso de no no ser así llamo a la funcion alerta, que abre una ventana de alerta
-        if(items[i] == null){
-            console.log("El item numero "+numerodeItem+" no tiene respuesta")
-            alertaValidacionRadios();
-            break
-        }
-    }
-    console.log(items)
+        //Aca divido el array item en dos array en base a la categoria de cada pregunta
+        //Al final asigne las variables al revés y quedó bien jaja
+        //Tampoco termino de entender por que despues del splice, se me duplica el elemento 13 del array items (por eso el .pop) 
+        let itemsAnsiedadSomatica = items.splice(6,7,items[13]);
+        items.pop()
+        let itemsAnsiedadPsiquica =items;
 
-    //Aca divido el array item en dos array en base a la categoria de cada pregunta
-    //Al final asigne las variables al revés y quedó bien jaja
-    //Tampoco termino de entender por que despues del splice, se me duplica el elemento 13 del array items (por eso el .pop) 
-    let itemsAnsiedadSomatica = items.splice(6,7,items[13]);
-    items.pop()
-    let itemsAnsiedadPsiquica =items;
-
-    console.log(itemsAnsiedadSomatica)
-    console.log("ansiedadPsi "+ itemsAnsiedadPsiquica)
-    console.log("ansiedadSo "+ itemsAnsiedadSomatica)
- 
-    //Sumo de los items de cada categoría
-    for(i=0; i < itemsAnsiedadPsiquica.length; i++){
-        ansiedadPsiquica = parseInt(ansiedadPsiquica) + parseInt(itemsAnsiedadPsiquica[i])
-    };
-    for(i=0; i < itemsAnsiedadSomatica.length; i++){
-        ansiedadSomatica = parseInt(ansiedadSomatica) + parseInt(itemsAnsiedadSomatica[i])
-    };
+        console.log(itemsAnsiedadSomatica)
+        console.log("ansiedadPsi "+ itemsAnsiedadPsiquica)
+        console.log("ansiedadSo "+ itemsAnsiedadSomatica)
     
-    //Asigno las propiedades al objeto
-    resultadosEscalaHars.aPsiquica=ansiedadPsiquica
-    resultadosEscalaHars.aSomatica=ansiedadSomatica
-    resultadosEscalaHars.ansiedadTotal=parseInt(ansiedadPsiquica)+parseInt(ansiedadSomatica)
-}
-
-
-//Esta funcion interpreta los resultados para devolver al usuario el tipo de síntomas que tiene el paciente
-function harsResultados(objetoEscalaHars){
-    let texto
-    switch (objetoEscalaHars.ansiedadTotal>-1){
-
-        case objetoEscalaHars.ansiedadTotal == 0:
-            texto = "El paciente no presenta sintomás de Ansiedad, sus puntajes son de 0 en todos los items.";
-            break;
-
-        case objetoEscalaHars.ansiedadTotal < 17:
-            texto ="El paciente posee niveles de severidad de Ansiedad leves:";
-            break;
-
-        case objetoEscalaHars.ansiedadTotal > 17 && objetoEscalaHars.ansiedadTotal < 24:
-            texto ="Los niveles de Ansiedad van de moderados a severos:";
-            break;
-
-        case objetoEscalaHars.ansiedadTotal > 24 && objetoEscalaHars.ansiedadTotal < 56:
-            texto =" Los niveles de Ansiedad son muy severos e incapacitantes:";
-            break;
+        //Sumo de los items de cada categoría
+        for(i=0; i < itemsAnsiedadPsiquica.length; i++){
+            ansiedadPsiquica = parseInt(ansiedadPsiquica) + parseInt(itemsAnsiedadPsiquica[i])
+        };
+        for(i=0; i < itemsAnsiedadSomatica.length; i++){
+            ansiedadSomatica = parseInt(ansiedadSomatica) + parseInt(itemsAnsiedadSomatica[i])
+        };
+        
+        //Asigno las propiedades al objeto
+        resultadosEscalaHars.aPsiquica=ansiedadPsiquica
+        resultadosEscalaHars.aSomatica=ansiedadSomatica
+        resultadosEscalaHars.ansiedadTotal=parseInt(ansiedadPsiquica)+parseInt(ansiedadSomatica)
     }
-    //imprimo el texto correspondiente en mi HTML
-    let textoNivelesDeAnsiedad= document.createTextNode(texto);
-    nivelesDeAnsiedadHtml.appendChild(textoNivelesDeAnsiedad);
 
-    //Si el puntaje es distinto de 0 los muestra en el HTML
-    if(objetoEscalaHars.ansiedadTotal != 0){
-        let texto1="Resultado Total: "+objetoEscalaHars.ansiedadTotal+" puntos"
-        let texto2="Ansiedad Psíquica: "+objetoEscalaHars.aPsiquica+" puntos";
-        let texto3="Ansiedad Somática: "+objetoEscalaHars.aSomatica+" puntos";
 
-        let textoResultadoTotal=document.createTextNode(texto1)
-        let textoAPsicquica=document.createTextNode(texto2);
-        let textoASomatica=document.createTextNode(texto3);
+    //Esta funcion interpreta los resultados para devolver al usuario el tipo de síntomas que tiene el paciente
+    function harsResultados(objetoEscalaHars){
+        let texto
+        switch (objetoEscalaHars.ansiedadTotal>-1){
 
-        resultadoPrincipalHtml.appendChild(textoResultadoTotal);
-        resultadoAPsiquicaHtml.appendChild(textoAPsicquica);
-        resultadoASomaticaHtml.appendChild(textoASomatica);
+            case objetoEscalaHars.ansiedadTotal == 0:
+                texto = "El paciente no presenta sintomás de Ansiedad, sus puntajes son de 0 en todos los items.";
+                break;
+
+            case objetoEscalaHars.ansiedadTotal < 17:
+                texto ="El paciente posee niveles de severidad de Ansiedad leves:";
+                break;
+
+            case objetoEscalaHars.ansiedadTotal > 17 && objetoEscalaHars.ansiedadTotal < 24:
+                texto ="Los niveles de Ansiedad van de moderados a severos:";
+                break;
+
+            case objetoEscalaHars.ansiedadTotal > 24 && objetoEscalaHars.ansiedadTotal < 56:
+                texto =" Los niveles de Ansiedad son muy severos e incapacitantes:";
+                break;
+        }
+        //imprimo el texto correspondiente en mi HTML
+        let textoNivelesDeAnsiedad= document.createTextNode(texto);
+        nivelesDeAnsiedadHtml.appendChild(textoNivelesDeAnsiedad);
+
+        //Si el puntaje es distinto de 0 los muestra en el HTML
+        if(objetoEscalaHars.ansiedadTotal != 0){
+            let texto1="Resultado Total: "+objetoEscalaHars.ansiedadTotal+" puntos"
+            let texto2="Ansiedad Psíquica: "+objetoEscalaHars.aPsiquica+" puntos";
+            let texto3="Ansiedad Somática: "+objetoEscalaHars.aSomatica+" puntos";
+
+            let textoResultadoTotal=document.createTextNode(texto1)
+            let textoAPsicquica=document.createTextNode(texto2);
+            let textoASomatica=document.createTextNode(texto3);
+
+            resultadoPrincipalHtml.appendChild(textoResultadoTotal);
+            resultadoAPsiquicaHtml.appendChild(textoAPsicquica);
+            resultadoASomaticaHtml.appendChild(textoASomatica);
+        }
+    
     }
- 
-}
 
 
-//Esta función es para agregar una ventana de alerta en mi HTML cuando falta algun item de responder
-function alertaValidacionRadios(){
-    const nodoMain = document.getElementById("main");
-    let agregarAlert= document.createElement('div');
+    //Esta función es para agregar una ventana de alerta en mi HTML cuando falta algun item de responder
+    function alertaValidacionRadios(){
+        const nodoMain = document.getElementById("main");
+        let agregarAlert= document.createElement('div');
 
-    agregarAlert.classList.add("alert","alert-danger","alert-dismissible","fade","show","alerta");
-    agregarAlert.setAttribute('role', 'alert');
-    agregarAlert.innerHTML = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Quedan Items sin responder!</strong>';
+        agregarAlert.classList.add("alert","alert-danger","alert-dismissible","fade","show","alerta");
+        agregarAlert.setAttribute('role', 'alert');
+        agregarAlert.innerHTML = '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Quedan Items sin responder!</strong>';
 
-    nodoMain.appendChild(agregarAlert)
+        nodoMain.appendChild(agregarAlert)
 
-}
+    }
