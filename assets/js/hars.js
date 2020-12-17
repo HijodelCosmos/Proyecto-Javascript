a73442c7efbbec64fb4f82a47728884accb9d41b
Nicolás Robles <nicopj20@gmail.com>
2020-12-16 21:21:12 -0300

    EUREKA! Logre que el DOM se actualice en tiempo real cuando modifico mi DataBase

--- assets/js/hars.js
+++ assets/js/hars.js
@@ -258,11 +258,23 @@ function aplicarDataTable() {
 
 
 //==============================BASE DE DATOS============================
+
+//Estas si o sí tienen que estar en funcion de flecha (no termino de entender por que)
 const getPacientes= () => db.collection("pacientes").get();
+const onGetPacientes= (callback)=> db.collection("pacientes").onSnapshot(callback);
+const getUnPaciente = (id) => db.collection("pacientes").doc(id).get()
+
+function editarPacienteDB(id,pacienteActualizado){
+    db.collection('pacientes').doc(id).update(pacienteActualizado)
+}
+//funcion para borrar de la base de datos
+function borrarPacienteDB(id){
+    db.collection('pacientes').doc(id).delete();
+}
 
 //Subo los obj pacientes a mi CloudFireStore 
 function subirPaciente(objPaciente){
-    let dbPaciente=db.collection('pacientes').doc().set({
+    db.collection('pacientes').doc().set({
         nombre: objPaciente.nombre,
         dni: objPaciente.dni,
         edad: objPaciente.edad,
@@ -275,159 +287,157 @@ function subirPaciente(objPaciente){
             'gravedadDeSintomas': resultadosEscalaHars.sintomas,
         }
     })
-    console.log(dbPaciente)
 }
-
-function editarPacienteDB(id,pacienteActualizado){
-    db.collection('pacientes').doc(id).update(pacienteActualizado)
-}
-//funcion para borrar de la base de datos los pacientes
-function borrarPacienteDB(id){
-    db.collection('pacientes').doc(id).delete();
-}
-
 
 //Obtener datos de la nube e insertarlos en la tabla
 window.addEventListener('DOMContentLoaded', async(e) => {
 
-    let querySnapshot = await getPacientes(); //Así denomínan los datos en la bibliografía de google
-    querySnapshot.forEach(doc => {
-        const paciente=doc.data();
-
-        //A esta variable la voy a usar para identificar de forma unica cada elemento en el dom que corresponde a cada paciente
-        paciente.id=doc.id; 
-
-        //Paso las primeras letras del nombre a Mayúsculas
-        let nombrePaciente = pasarStrMayuscula(paciente.nombre)
-
-        //Inserto en el DOM los datos de los pacientes dentro de una tabla
-        tablaPacientes.append(
-            `<tr class="rowPaciente">
-                <th>${nombrePaciente}</th>
-                <td>${paciente.dni}</td>
-                <td>${paciente.edad}</td>
-                <td>${paciente.sexo}</td>
-                <td>${paciente.eMail}</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalPaciente${paciente.id}">
-                    Más
-                    </button>
-                    <div class="modal" id="modalPaciente${paciente.id}" tabindex="-1" role="dialog" aria-labelledby="modalPacienteLabel" aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                            <h3 class="modal-title" id="modalPacienteLabel${paciente.id}">${nombrePaciente}</h3>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                            </div>
-                            <div id="modal-body-${paciente.id}" class="modal-body">
-                            <div id="modalBodyDatos${paciente.id}">
-                                <dl>
-                                    <dt>Datos Personales:</dt>
-                                    <dd class="ml-3">DNI: ${paciente.dni}</dd>
-                                    <dd class="ml-3">Edad: ${paciente.edad}</dd>
-                                    <dd class="ml-3">Sexo: ${paciente.sexo}</dd>
-                                    <dd class="ml-3">Email: ${paciente.eMail}</dd>
-                                </dl>
-                                <dl>
-                                    <dt>Resultados Escala Hars:</dt>
-                                    <dd class="ml-3">${paciente.escalaHars.gravedadDeSintomas}</dd>
-                                    <dd class="ml-3">Ansiedad Total: ${paciente.escalaHars.resultadoAnsiedadTotal}</dd>
-                                    <dd class="ml-3">Ansiedad Psíquica: ${paciente.escalaHars.resultadoAnsiedadPsiquica}</dd>
-                                    <dd class="ml-3">Ansiedad Psíquica: ${paciente.escalaHars.resultadoAnsiedadSomatica}</dd>
-                                </dl>
-                            </div>
-                            </div>
-                            <div class="modal-footer" id="modal-footer-${paciente.id}">
-                            <div id="bnts-datos-${paciente.id}">
-                                <button type="button" class="btn btn-primary btnEditar" data-id="${paciente.id}">Editar</button>
-                                <button type="button" class="btn btn-danger btnBorrar" data-id="${paciente.id}" data-dismiss="modal">Borrar</button>
+    await onGetPacientes((querySnapshot)=>{
+        tablaPacientes.html('')
+        //Este forEach recorre cada objeto paciente traido de la nube y le crea todos sus elementos en el DOM
+        querySnapshot.forEach(doc => {
+            const paciente=doc.data();
+    
+            //Identifica de forma unica cada elemento en el dom que corresponde a cada paciente
+            paciente.id=doc.id; 
+    
+            //Paso las primeras letras del nombre a Mayúsculas EJ: Pepito Pedro
+            let nombrePaciente = pasarStrMayuscula(paciente.nombre)
+    
+            //Inserto en el DOM los datos de los pacientes dentro de una tabla
+            tablaPacientes.append(
+                `<tr class="rowPaciente">
+                    <th>${nombrePaciente}</th>
+                    <td>${paciente.dni}</td>
+                    <td>${paciente.edad}</td>
+                    <td>${paciente.sexo}</td>
+                    <td>${paciente.eMail}</td>
+                    <td>
+                        <button type="button" class="btnTrigger btn btn-primary" data-id="${paciente.id}" data-toggle="modal" data-target="#modalPaciente${paciente.id}">
+                        Más
+                        </button>
+                        <div class="modal" id="modalPaciente${paciente.id}" tabindex="-1" role="dialog" aria-labelledby="modalPacienteLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                <h3 class="modal-title" id="modalPacienteLabel${paciente.id}">${nombrePaciente}</h3>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                </div>
+                                <div id="modal-body-${paciente.id}" class="modal-body">
+                                <div id="modalBodyDatos${paciente.id}">
+                                    <dl>
+                                        <dt>Datos Personales:</dt>
+                                        <dd class="ml-3">Nombre: ${nombrePaciente}</dd>
+                                        <dd class="ml-3">DNI: ${paciente.dni}</dd>
+                                        <dd class="ml-3">Edad: ${paciente.edad}</dd>
+                                        <dd class="ml-3">Sexo: ${paciente.sexo}</dd>
+                                        <dd class="ml-3">Email: ${paciente.eMail}</dd>
+                                    </dl>
+                                    <dl>
+                                        <dt>Resultados Escala Hars:</dt>
+                                        <dd class="ml-3">${paciente.escalaHars.gravedadDeSintomas}</dd>
+                                        <dd class="ml-3">Ansiedad Total: ${paciente.escalaHars.resultadoAnsiedadTotal}</dd>
+                                        <dd class="ml-3">Ansiedad Psíquica: ${paciente.escalaHars.resultadoAnsiedadPsiquica}</dd>
+                                        <dd class="ml-3">Ansiedad Psíquica: ${paciente.escalaHars.resultadoAnsiedadSomatica}</dd>
+                                    </dl>
+                                </div>
+                                </div>
+                                <div class="modal-footer" id="modal-footer-${paciente.id}">
+                                <div id="bnts-datos-${paciente.id}">
+                                    <button type="button" class="btn btn-primary btnEditar" data-id="${paciente.id}">Editar</button>
+                                    <button type="button" class="btn btn-danger btnBorrar" data-id="${paciente.id}" data-dismiss="modal">Borrar</button>
+                                </div>
+                                </div>
                             </div>
                             </div>
                         </div>
-                        </div>
-                    </div>
-                </td>
-            </tr>`)
+                    </td>
+                </tr>`)
+    
+            //Inserto un form para Editar los Datos y lo oculto
+            $(`#modal-body-${paciente.id}`).append(`<form id="formModalPaciente-${paciente.id}" class="justify-content-center align-content-center">
+                <input id="inputNombre-${paciente.id}" type="text" class="inputPaciente d-block mx-auto" aria-describedby="nombrePaciente" value="${nombrePaciente}">
+                <input id="inputDni-${paciente.id}" type="number" class="inputPaciente d-block mx-auto" aria-describedby="dniPaciente" value="${paciente.dni}">
+                <input id="inputEdad-${paciente.id}" type="number" class="inputPaciente d-block mx-auto inputEdad" aria-describedby="edadPaciente" value="${paciente.edad}">
+                <select id="inputSexo-${paciente.id}" class="d-block mx-auto inputSexo" name="sexo">
+                    <option value="${paciente.sexo}" selected hidden class="etiquetaSelect mx-auto">${paciente.sexo}</option>
+                    <option value="Femenino" >Femenino</option>
+                    <option value="Masculino">Masculino</option>
+                    <option value="Otro">Otro</option>
+                </select>
+                <input id="inputEmail-${paciente.id}" type="email" class="inputPaciente d-block mx-auto" value="${paciente.eMail}">
+                </form>`);
+            $(`#formModalPaciente-${paciente.id}`).hide();
+    
+            //Inserto botones para el form editar y los oculto
+            $(`#modal-footer-${paciente.id}`).append(
+                `<div id="btns-editar-${paciente.id}">
+                    <button type="button" class="btn btn-primary btnGuardar" data-id="${paciente.id}" data-dismiss="modal">Guardar Cambios</button>
+                    <button type="button" class="btn btn-danger btnCancelar" data-id="${paciente.id}" data-dismiss="modal">Cancelar</button>
+                </div>`);
+            $(`#btns-editar-${paciente.id}`).hide();
 
-        //Inserto un form para Editar los Datos y lo oculto
-        $(`#modal-body-${paciente.id}`).append(`<form id="formModalPaciente-${paciente.id}" class="justify-content-center align-content-center">
-        <input id="inputNombre-${paciente.id}" type="text" class="inputPaciente d-block mx-auto" aria-describedby="nombrePaciente" placeholder="${nombrePaciente}">
-        <input id="inputDni-${paciente.id}" type="number" class="inputPaciente d-block mx-auto" aria-describedby="dniPaciente" placeholder="${paciente.dni}">
-        <input id="inputEdad-${paciente.id}" type="number" class="inputPaciente d-block mx-auto inputEdad" aria-describedby="edadPaciente" placeholder="${paciente.edad}">
-        <select id="inputSexo-${paciente.id}" class="d-block mx-auto inputSexo" name="sexo">
-            <option value="" disabled selected hidden class="etiquetaSelect mx-auto">${paciente.sexo}</option>
-            <option value="Femenino" >Femenino</option>
-            <option value="Masculino">Masculino</option>
-            <option value="Otro">Otro</option>
-        </select>
-        <input id="inputEmail-${paciente.id}" type="email" class="inputPaciente d-block mx-auto" placeholder="${paciente.eMail}">
-        </form>`);
-        $(`#formModalPaciente-${paciente.id}`).hide();
-
-        //Inserto botones para el form editar y los oculto
-        $(`#modal-footer-${paciente.id}`).append(
-            `<div id="btns-editar-${paciente.id}">
-                <button type="button" class="btn btn-primary btnGuardar" data-id="${paciente.id}" data-dismiss="modal">Guardar Cambios</button>
-                <button type="button" class="btn btn-danger btnCancelar" data-id="${paciente.id}" data-dismiss="modal">Cancelar</button>
-            </div>`);
-        $(`#btns-editar-${paciente.id}`).hide();
-    })
-
-
-    //Borrar datos
-    const btnsBorrar = document.querySelectorAll('.btnBorrar'); //Si lo selecciono con jQuery el forEach no lo interpreta como un array
-    //Este evento recorre todos los botones de borrar de cada paciente y dispara la funcion borrar de la DB
-    btnsBorrar.forEach(btn => {
-        btn.addEventListener('click',(e)=>{
-            borrarPacienteDB(e.target.dataset.id)
         })
-    })
 
-    //Cambiar el modal a modo Edicion
-    const btnsEditar = document.querySelectorAll(".btnEditar")
-    btnsEditar.forEach(btn => {
-        btn.addEventListener('click', e =>{
-            let id= e.target.dataset.id
-            //Primero cambio la interfase del DOM
-            $(`#modalBodyDatos${id}`).fadeOut();
-            $(`#bnts-datos-${id}`).fadeOut();
-            $(`#formModalPaciente-${id}`).delay(1000);
-            $(`#formModalPaciente-${id}`).fadeIn(1000);
-            $(`#btns-editar-${id}`).delay(1000);
-            $(`#btns-editar-${id}`).fadeIn();
-            $(`#modalPacienteLabel${id}`).html('Editar datos del Paciente');
+
+        //Borrar datos
+        const btnsBorrar = document.querySelectorAll('.btnBorrar'); //Si lo selecciono con jQuery el forEach no lo interpreta como un array
+        //Este evento recorre todos los botones de borrar de cada paciente y dispara la funcion borrar de la DB
+        btnsBorrar.forEach(btn => {
+            btn.addEventListener('click',async (e) =>{
+                await borrarPacienteDB(e.target.dataset.id);
+            })
         })
-    })
-
-    //Guardar datos Editados
-    const btnsGuardar = document.querySelectorAll(".btnGuardar");
-    btnsGuardar.forEach(btn => {
-        btn.addEventListener('click',e =>{
-            let id= e.target.dataset.id;
-            editarPacienteDB(id,{
-                nombre:$(`#inputNombre-${id}`).val(),
-                dni: $(`#inputDni-${id}`).val(),
-                edad: $(`#inputEdad-${id}`).val(),
-                sexo: $(`#inputSexo-${id}`).val(),
-                eMail: $(`#inputEmail-${id}`).val(),})
-                })
-    })
-
-    //Cerrar el modal y volver a la interfase de datos en vez del Form para editar
-    const btnsCancelar = document.querySelectorAll(".btnCancelar");
-    btnsCancelar.forEach(btn => {
-        btn.addEventListener('click', e =>{
-            let id= e.target.dataset.id;
-            $(`#formModalPaciente-${id}`).hide();
-            $(`#btns-editar-${id}`).hide();
-            $(`#modalBodyDatos${id}`).show();
-            $(`#bnts-datos-${id}`).show();
-            $(`#modalPacienteLabel${id}`).html(`${nombrePaciente}`);//<== CORREGIR hacer una funcion getpaciente que me baje el nombre
+        //Cambiar el modal a modo Edicion
+        const btnsEditar = document.querySelectorAll(".btnEditar")
+        btnsEditar.forEach(btn => {
+            btn.addEventListener('click', e =>{
+                let id= e.target.dataset.id
+                //Primero cambio la interfase del DOM
+                $(`#modalBodyDatos${id}`).fadeOut();
+                $(`#bnts-datos-${id}`).fadeOut();
+                $(`#formModalPaciente-${id}`).delay(1000);
+                $(`#formModalPaciente-${id}`).fadeIn(1000);
+                $(`#btns-editar-${id}`).delay(1000);
+                $(`#btns-editar-${id}`).fadeIn();
+                $(`#modalPacienteLabel${id}`).html('Editar datos del Paciente');
+            })
         })
+
+        //Guardar datos Editados
+        const btnsGuardar = document.querySelectorAll(".btnGuardar");
+        btnsGuardar.forEach(btn => {
+            btn.addEventListener('click',async(e) =>{
+                let id= e.target.dataset.id;
+                await editarPacienteDB(id,{
+                    nombre:$(`#inputNombre-${id}`).val(),
+                    dni: $(`#inputDni-${id}`).val(),
+                    edad: $(`#inputEdad-${id}`).val(),
+                    sexo: $(`#inputSexo-${id}`).val(),
+                    eMail: $(`#inputEmail-${id}`).val(),})
+            })
+        })
+
+        //Cerrar el modal y volver a la interfase de datos en vez del Form para editar
+        const btnsCancelar = document.querySelectorAll(".btnCancelar");
+        btnsCancelar.forEach(btn => {
+            btn.addEventListener('click', async(e) =>{
+                let id= e.target.dataset.id;
+                //Esto es para vovler a poner el nombre en el modal
+                let doc= await getUnPaciente(id)
+                let nombre = pasarStrMayuscula(doc.data().nombre)
+
+                $(`#formModalPaciente-${id}`).hide();
+                $(`#btns-editar-${id}`).hide();
+                $(`#modalBodyDatos${id}`).show();
+                $(`#bnts-datos-${id}`).show();
+                $(`#modalPacienteLabel${id}`).html(`${nombre}`);
+            })
+        })
+
     })
-    aplicarDataTable()
 })
 
 
